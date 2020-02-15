66551a1f7a2c01b6f671997e2f1a6bd96d9c2100
m2jm2j <37734226+marcin2japonia@users.noreply.github.com>
2020-02-15 19:21:06 +0100

    Create temp.py
    Dodanie programu piszącego SPAM

---
+++ temp.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+
+
+
+spam_amount = 0
+print(spam_amount)
+
+spam_amount = spam_amount + 4
+
+if spam_amount > 0:
+    print("But I don't want any spam!")
+
+viking_song = "Spam " * spam_amount
+print(viking_song)
+
+print("Spam amount ", spam_amount)
+print(min(1,2,3))
